0fc7e898cbddb510a6cee75036adbb52cd54f189
IDKWTW <40872054+IDKWTW@users.noreply.github.com>
2018-07-30 09:47:44 -0700

    Add files via upload

---
+++ cyo_burgers_and_fries.py
@@ -0,0 +1,119 @@
+print ('''You leave your bedroom and run into your roommate in the living room.
+She is your party friend of the group.
+She’s known for being mildly irresponsible and is VERY go-with-the-flow.
+She wants you to go out to a new bar with her.
+She says you could meet guys there, and it sounds like a nice break from school.
+You agree to go to the bar later, but you’re already late for work.
+You finish getting ready and head out the door.
+You arrive at the news station twenty minutes after you were expected to.
+As an intern, you were supposed to have the day’s reports ready and on their desks.
+As you’re laying them out, Jeff approaches you.''')
+print ('''You’ve only known him for a month or so.
+You’ve caught him staring at you from his desk before, and the other interns have warned you to stay away from him.
+He suddenly hugs you as he says, “Good morning!”.
+''')
+yn = input("Do you push him off?")
+if yn == "yes":
+    print ('''You quickly push Jeff away from you.
+He’s visibly angry, but his expression clears.
+He says, “Mr. Parker’s waiting for you in his office.”
+As you walk towards your supervisor’s office, Jeff grabs your wrist.
+“Wait a minute,” he says.
+You ask what he wants, and he whispers, “good luck” before you pull away and walk to your supervisor’s office.''')
+else:
+    print (''' Jeff holds you for a few uncomfortable seconds before letting go.
+He looks a little concerned while he says, “Mr. Parker’s waiting for you in his office.”
+As you’re walking towards your supervisor’s office, Jeff calls out “good luck!”
+You turn back to him as you reach the office, and Jeff is still watching you.''')
+print ('''You knock on the door, and Mr. Parker calls you in.
+He’s a well-respected man, and he has helped a lot of interns reach their ultimate goal of becoming full-time reporters or news anchors.
+You take a seat across from him, and he begins to speak.
+“You know, it takes everyone doing their work to keep this place afloat.
+And you’ve been late at least four times this month. Do you have a reason?” he asks.
+You try to remember the previous night, but there’s a section of the night that just seems to be missing.
+You don’t want your boss to think you’re irresponsible.''')
+yon = input("Should you tell him?")
+if yon == "no":
+    print ('''”I...I don’t know what happened today, Mr. Parker. But I promise, it won’t happen again,” you say.''')
+else:
+    print (''' “I...I can explain the other incidents, Mr. Parker, but I don’t know what happened this morning,” you admit.
+“You don’t know what happened?” he asks.
+“No. I went to a concert last night, I was dancing with my friends, and then there’s a whole part of the night that’s missing.
+I didn’t even have a drink,” you say.''')
+print ('''“I’m a gracious man, but I can’t allow so many mistakes from one member of this team,” he admits.
+Mr. Parker sits on his desk in front of you and continues:
+“I could ignore your late arrivals, but you need to make up your missing time.”
+You tell Mr. Parker that your work can’t cut into anymore of your classes.
+“That won’t be an issue. We can arrange something...you can make up the time you missed another way,” he says, putting a hand on your knee.
+Your body freezes, and you can’t believe what you’re hearing.''')
+yorn = input("Should you try to reason with Mr. Parker or quit?")
+if yorn == "reason":
+    print ('''You remove Mr. Parker’s hand and take a few steps towards the door.
+“Mr. Parker, I really want to stay at this station, but I just can’t do that,” you say.
+“What do you want to be, huh? Journalist? News anchor?
+You really think you can do that all by yourself?
+I could make or break your career,” he says.
+You rush out the door, and Mr. Parker yells after you, “Don’t come back!”''')
+else:
+    print ('''You quickly remove Mr. Parker’s hand and take a few steps towards the door.
+“I can’t do that, Mr. Parker,” you say.
+“What do you want to be, huh? Journalist? News anchor? You really think you can do that all by yourself?” he says.
+“I don’t know,” you say, “but I can’t stay at this station, either.”
+You rush out the door, and Mr. Parker yells after you, “You’re finished!”''')
+print ('''As you’re walking back to your desk, you’re still a little shocked that this is happening.
+First it was Jeff, and now Mr. Parker?
+You always knew things like this happened, and you should have known it could happen to you, too.
+Mr. Parker does carry a lot of influence in the news business, but you will not let him do this to anyone else.
+Even though you weren’t being paid a lot, you still need the money. Your scholarship doesn’t cover everything…
+While you pack the belongings on your desk, another intern shows up.
+She gives you a weak smile and asks if you would like help with packing.
+You accept.
+“My name is Emma. I’m also an intern,” she says.
+You introduce yourself to Emma.
+“Did Mr. Parker make you an offer?” she asks.
+You look up suddenly, and she looks away.
+“I think you did the right thing...saying no. Mr. Parker thinks he owns us, but he doesn’t. We can’t be afraid of him,” she says.
+“I won’t let him do this to anyone else,” you say.
+Emma smiles hopefully. “Human Resources could help you if you filed a complaint, and we could even talk to former interns about their experiences.”''')
+yesrno = input ("Do you file a complaint before you leave?")
+if yesrno == yes
+    print ('''You take Emma’s advice and file a complaint with Human Resources.
+You ask if anyone else has filed a complaint against Mr. Parker, but they tell you that information is private.
+They do tell you that they’ll be in contact.
+You meet Emma near the entrance, and she has brought two other interns with her.
+You all exchange contact information and plan for a future meeting.
+You leave the news station.''')
+else:
+    print (''' You can’t seem to understand what good it would do to file a complaint, so you decide not to.
+Just imagine the damage that Mr. Parker could do to your career.
+Who would listen to you anyway?
+You think about the former interns...one of them must have filed a complaint, right?
+You walk past Emma as you leave the news station.''')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
