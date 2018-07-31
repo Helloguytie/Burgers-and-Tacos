d3ffda605493e43309c547721484122c0121ac46
Sophie <sophiebayrouk@gmail.com>
2018-07-31 13:09:20 -0700

    hope

--- Combined.py
+++ Combined.py
@@ -1,19 +1,18 @@
-def space():
-    i = 0
-    while i < 2:
-        print()
-        i += 1
+def space_line():
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
 def intro_code():
+    space_line()
     print('''You leave your bedroom and run into your roommate in the living room. She is your party friend of the group. She’s known for being mildly irresponsible and is VERY go-with-the-flow. She wants you to go out to a new bar with her. She says you could meet guys there, and it sounds like a nice break from school. You agree to go to the bar later, but right now you’re already late for work. You finish getting ready and head out the door. You arrive at the news station twenty minutes after you were expected to. As an intern, you were supposed to have the day’s reports ready and on their desks. As you’re laying them out, your colleague, Jeff, approaches you. You’ve only known him for a month or so. You sometimes catch him staring at you from his desk, but your colleagues only have good things to say about him. He suddenly hugs you as he says, “Good morning!”''')
 
 def end_code():
-    space()
-    space()
+    space_line()
     print('''Thank you for playing. "How Would You React?” is the final project from the 2018 Girls Who Code Summer Immersion Program at AT&T: Los Angeles. It was created by five students: Aliyah McDaniel, Jessica Moreno, Shawahnah Haraway, Sophie Gutierrez, and Zari Williams. Aliyah will be a junior at Mira Costa High School. Jessica is a rising junior at Animo Pat Brown Charter High School. Shawahnah is homeschooled and is going to become a junior this year. Sophie is a rising junior at Da Vinci Science High School. She enjoys playing choose-your-own adventure games, and one of her favorite games is Telltale Games’ The Walking Dead. Zari will be a senior attending Alexander Hamilton Senior High School. Her favorite part of coding is being able to control what she wants to happen and being behind the scenes. The website for “How Would You React?” was coded using HTML, CSS, and JavaScript. “How Would You React?” was coded using Python. If you or someone you know has been a victim of sexual assault, resources include  800.656.HOPE (4673), rainn.org, projectcallisto.org, and (202) 467-8700 (National Center for Victims of Crime). If you or someone you know has been a victim of sexual harassment or discrimination in the workplace, a resource includes 1-800-522-0925 (9 to 5: National Association of Working Women). For immediate help, contact the National Sexual Assault Hotline at 1-800-656-4673.''')
 
 def naive():
-    space()
-    space()
+    space_line()
     intro_code()
     new1_dict = []
 
@@ -36,9 +35,11 @@ def naive():
             tell1 = input("Should you tell him? a) yes or b) no")
             if tell1 == "a":
                 second_condition = False
+                space_line()
                 print('''“I...I can explain the other incidents, Mr. Parker, but I don’t know what happened this morning,” you admit. “You don’t know what happened?” he asks. “No. I went to a concert last night, I was dancing with my friends, and then there’s a whole part of the night that’s missing. I didn’t even have a drink,” you say.''')
             elif tell1 == "b":
                 second_condition = False
+                space_line()
                 print('''”I...I don’t know what happened today, Mr. Parker. But I promise, it won’t happen again,” you say.''')
             else:
                 print("You did not put a valid option.")
@@ -49,12 +50,14 @@ def naive():
             boss1 = input("Should you try to reason with Mr. Parker or quit? a) reason or b) quit")
             if boss1 == "a":
                 third_condition = False
+                space_line()
                 print('''You remove Mr. Parker’s hand and take a few steps towards the door.
                 “Mr. Parker, I really want to stay at this station, but I just can’t do that,” you say.
                 “What do you want to be, huh? Journalist? News anchor? You really think you can do that all by yourself? I could make or break your career,” he says.
                 You rush out the door, and Mr. Parker yells after you, “Don’t come back!”''')
             elif boss1 == "b":
                 third_condition = False
+                space_line()
                 print('''You quickly remove Mr. Parker’s hand and take a few steps towards the door.
                 “I can’t do that, Mr. Parker,” you say.
                 “What do you want to be, huh? Journalist? News anchor? You really think you can do that all by yourself?” he says.
@@ -69,9 +72,11 @@ def naive():
             complaint1 = input("Do you file a complaint before you leave? a) yes or b) no")
             if complaint1 == "a":
                 fourth_condition = False
+                space_line()
                 print('''You take Emma’s advice and file a complaint with Human Resources. They tell you that they’ll be in contact, and you leave the news station.''')
             elif complaint1 == "b":
                 fourth_condition = False
+                space_line()
                 print('''You can’t seem to understand what good it would do to file a complaint, so you decide not to. You leave the news station.''')
             else:
                 print("You did not put a valid option.")
@@ -82,6 +87,7 @@ def naive():
             eavesdrop1_continued = input("'Wasn’t it great?'' one of your classmates asks. a) yes or b) no")
             if eavesdrop1_continued == "a":
                 fifth_condition = False
+                space_line()
                 print('''You talk about the great time you had, and the other classmate adds, “From your posts, it sure looked like you had a good time.”
                 You thought there was only one picture...what else did you post?
                 You enter the lecture hall and take a seat, and it doesn’t take long before the whispering starts.
@@ -89,6 +95,7 @@ def naive():
                 There’s a few whistles, but the professor quiets the room and no one will meet your eye. You decide to leave class early.''')
             elif eavesdrop1_continued == "b":
                 fifth_condition = False
+                space_line()
                 print('''You admit that you don’t really remember what happened at the concert.
                 “Sure, sure. I never thought you’d let yourself go like that. You didn’t strike me as a party animal.
                 Have you called that guy back yet?” one classmate asks.
@@ -106,11 +113,13 @@ def naive():
             eavesdrop1 = input("Do you continue walking or wait where you are? a) continue or b) wait")
             if eavesdrop1 == "a":
                 sixth_condition = False
+                space_line()
                 print('''You continue walking and run into your two classmates.
                 “Hey, you. We were just talking about the concert last night."''')
                 eavesdrop1_continued_code()
             elif eavesdrop1 == "b":
                 sixth_condition = False
+                space_line()
                 print('''"You wait in the hallway and listen to your classmates, who are unaware that you’re just around the corner.
                 “That was a crazy concert. Some people really let themselves go last night,” one classmate says. They begin to talk about you.
                 “Did you see the guy she was with? I don’t think he goes here, and he never let her out of his sight.”
@@ -136,10 +145,12 @@ def naive():
             if leave == "a":
                 seventh_condition = False
                 new1_dict.append("left")
+                space_line()
                 print('''You leave your drink and go to the dance floor with your friend. You and your friend move further into the crowd. You can no longer see the bar or your drink, but your friend is really excited.''')
             elif leave == "b":
                 seventh_condition = False
                 new1_dict.append("didn't leave")
+                space_line()
                 print("You drink the rest of your drink before joining your friend on the dance floor.")
             else:
                 print("You did not put a valid option.")
@@ -149,12 +160,14 @@ def naive():
             drink1 = input("Do you accept his drink? a) yes or b) no")
             if drink1 == "a":
                 eighth_condition = False
+                space_line()
                 print("You accept the drink, and the man cuts you off as you begin to order. You think to yourself, 'If he’s buying, he’s ordering, right?'")
                 print()
                 print('''“That wasn’t so bad, huh?” your friend asks. She invites you to join her on the dance floor. You think about leaving the drink at the bar in order to dance. You’re sure it’ll be safe, and you’ll only be gone for a little while.''')
                 leave_it_code()
             elif drink1 == "b":
                 eighth_condition = False
+                space_line()
                 print('''You decline the drink, and the man yells, “But I already bought it!” You decline again, and he says, “It could be the only offer you get tonight, sweetheart, but fine.” You buy yourself a drink instead.''')
                 print(''' “That guy was rude, but don’t let it spoil your night,” your friend says. She invites you to join her on the dance floor. You think about leaving the drink at the bar in order to dance. You’re sure it’ll be safe, and you’ll only be gone for a little while.''')
                 leave_it_code()
@@ -166,9 +179,11 @@ def naive():
             dance1 = input("Do you dance with him? a) yes or b) no")
             if dance1 == "a":
                 ninth_condition = False
+                space_line()
                 print('''You agree to dance with him. He grabs your wrist a little too tightly and leads you to the dance floor. He probably just doesn’t want to lose you in the crowd. You make it back to your friend after dancing with him for a while.''')
             elif dance1 == "b":
                 ninth_condition = False
+                space_line()
                 print("You decline his offer. He glares at you for a few seconds, but his expression softens. He puts his hand on your shoulder and says, “I’m never this forward, but you look great, and I just had to ask.” You thank him, move closer to your friend, and the man walks away.")
             else:
                 print("You did not put a valid option.")
@@ -192,46 +207,53 @@ def naive():
                 print("You did not put a valid option.")
 
     hug1_code()
+    space_line()
     print('''You knock on the door, and Mr. Parker calls you in. He’s a well-respected man, and he has helped a lot of interns reach their ultimate goal of becoming full-time reporters or news anchors. You take a seat across from him, and he begins to speak. “You know, it takes everyone doing their work to keep this place afloat. And you’ve been late at least four times this month. Do you have a reason?” he asks. You try to remember the previous night, but there’s a section of the night that just seems to be missing. You don’t want your boss to think you’re irresponsible.''')
     tell1_code()
+    space_line()
     print('''
     “I’m a gracious man, but I can’t allow so many mistakes from one member of this team,” he admits.
     Mr. Parker sits on his desk in front of you and continues: “I could ignore your late arrivals, but you need to make up your missing time.”
     You tell Mr. Parker that your work can’t cut into anymore of your classes.
     “That won’t be an issue. We can arrange something...you can make up the time you missed another way,” he says, putting a hand on your knee.
     Your body freezes, and you can’t believe what you’re hearing.''')
-    boss1_code()
+    boss1_code
+    space_line()
     print('''As you’re walking back to your desk, you’re still shocked that this is happening. First it was Jeff, and now Mr. Parker? You never thought something like this could happen to you. What do you do now? You weren’t being paid a lot, but you need the money. Your scholarship doesn’t cover everything…''')
-    print()
     print('''While you pack the belongings on your desk, another intern shows up. She gives you a weak smile and asks if you would like help with packing. You accept.
     “My name is Emma. I’m also an intern,” she says. You introduce yourself to Emma.
     “Did Mr. Parker make you an offer?” she asks. You look up suddenly, and she looks away.
     “I think you did the right thing...saying no. Mr. Parker thinks he owns us, but he doesn’t. We can’t be afraid of him,” she says.''')
-    print()
+    space_line()
     print('''“I don’t know what to do,” you say. Emma lets out a sigh, and her shoulders sag a little. “Maybe you could file a complaint before you leave? Human Resources could help you.”''')
     complaint1_code()
+    space_line()
     print("You arrive at "+college+" just in time for your first class. As you’re nearing the corner of a hallway, you hear two of your classmates talking. One of your classmates mentions your name, and you stop walking before you round the corner.")
     eavesdrop1_code()
+    space_line()
     print('''You return to your dorm and tell your roommate about your day. How could it have gone so wrong? Last night must be the cause of your problems.
     You ask your friend what happened, but she doesn’t have any new information. She manages to convince you to go to the bar with her later that night.
     Maybe it will take your mind off of the recent chain of events. You arrive and take a seat at the bar with your friend. Several minutes pass before you are offered a drink from a nicely-dressed, good-looking man. He seems trustworthy.''')
     drink1_code()
+    space_line()
     print('''You return to the bar after a few songs have played. The guy from earlier approaches you and asks you to dance. He hasn’t been a perfect gentleman, but you could give him a second chance.''')
     dance1_code()
     if "left" in new1_dict:
+        space_line()
         print("You sit back down at the bar and reach for your drink. It doesn’t look like there is anything wrong with it, and you were able to watch it from the dance floor.")
         spiked1_code()
     elif "didn't leave" in new1_dict:
+        space_line()
         print("You sit back down at the bar and have a conversation with your friend. Your friend tells you to wait for her while she uses the restroom.")
-        print()
+        space_line()
         print('''You and your friend leave the bar and return to your dorm.
         She apologizes for the day you’ve been through, but she knows you’ll get through it. As you lay in bed, you reflect on your day.
         You think about the things you could have done differently, and you realize that you didn’t have a lot of decisions to make.
         Some things seem to be just out of your control, but you were able to do something about it.''')
+        space_line()
     end_code()
 def aware():
-    space()
-    space()
+    space_line()
     intro_code()
     new_dict = []
 
@@ -476,8 +498,7 @@ def aware():
         Some things seem to be just out of your control, but you were able to do something about it.''')
     end_code()
 def woke():
-    space()
-    space()
+    space_line()
     intro_code()
     new2_dict = []
 
@@ -705,13 +726,6 @@ def woke():
         spiked2_code()
         end_code()
 
-college = input("What college do you want to attend?")
-print ("You are a 20 year old female that is currently attending "+college+".")
-print("You live in a dorm with your friends and experience various challeneges everyday, but today is different...")
-space()
-print ("You get to decide your fate! Choose wisely.")
-print ("You are in your dorm room. You went to a concert, and you remember that a person bumped into you after the concert. Your wallet is missing.")
-
 naive_count = 0
 aware_count = 0
 woke_count = 0
@@ -742,16 +756,19 @@ def window_code ():
             condition2 = False
             naive_count += 1
             print ("*breathes in* 'It's a nice day out', you say.")
+            space_line()
             door_code()
         elif window == "e":
             condition2 = False
             aware_count += 1
             print ("'There's a lot of people out there', you say as you close the window.")
+            space_line()
             door_code()
         elif window == "f":
             condition2 = False
             woke_count += 1
             print ("'There are too many people out there', you say as you lock the window.")
+            space_line()
             door_code()
         else:
             print("You did not put in a valid option.")
@@ -767,7 +784,7 @@ def door_code():
             condition3 = False
             naive_count += 1
             print ("'My roommate probably forgot', you say and walk back to your bed.")
-            space()
+            space_line()
             print ("You check your phone and see that your family has left you texts and voicemails.")
             print ("They tell you to check your Instagram.")
             photo_code()
@@ -775,7 +792,7 @@ def door_code():
             condition3 = False
             aware_count += 1
             print ("'I should close it' you say as walk over to the door and walk back over to your bed.")
-            space()
+            space_line()
             print ("You check your phone and see that your family has left you texts and voicemails.")
             print ("They tell you to check your Instagram.")
             photo_code()
@@ -783,7 +800,7 @@ def door_code():
             condition3 = False
             woke_count += 1
             print ("'I defintely closed this before I slept', you say as you lock the door and walk back over to your bed.")
-            space()
+            space_line()
             print ("You check your phone and see that your family has left you texts and voicemails.")
             print ("They tell you to check your Instagram.")
             photo_code()
@@ -826,21 +843,30 @@ def wallet_code ():
         if wallet == "a":
             condition1 = False
             naive_count += 1
+            space_line()
             print ("You notice that your window is open.")
             window_code()
         elif wallet == "b":
             condition1 = False
             aware_count += 1
+            space_line()
             print ("You notice that your window is open.")
             window_code()
         elif wallet == "c":
             condition1 = False
             woke_count += 1
+            space_line()
             print ("You notice that your window is open.")
             window_code()
         else:
             print("You did not put in a valid option.")
 def main():
+    college = input("What college do you want to attend?")
+    print ("You are a 20 year old female that is currently attending "+college+".")
+    print("You live in a dorm with your friends and experience various challenges everyday, but today is different...")
+    print ("You get to decide your fate! Choose wisely.")
+    space_line()
+    print ("You are in your dorm room. You went to a concert, and you remember that a person bumped into you after the concert. Your wallet is missing.")
     wallet_code()
 
 
